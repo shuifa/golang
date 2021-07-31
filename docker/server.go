5e1ddabec06f603037a7e1ad8a4bff2723ba7f0c
oushuifa <363717177@qq.com>
2021-07-31 18:09:28 +0800

    docker

--- docker/server.go
+++ docker/server.go
@@ -1,5 +1,25 @@
 package main
 
+import (
+    "fmt"
+    "html"
+    "log"
+    "net/http"
+)
+
 func main() {
-	$END$
+    fmt.Println("launch server at 8080 port")
+
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        _, err := fmt.Fprintf(w, "hello %q", html.EscapeString(r.URL.Path))
+        if err != nil {
+            log.Fatal(err)
+        }
+    })
+
+    err := http.ListenAndServe(":8080", nil)
+    if err != nil {
+        log.Fatal(err)
+    }
+
 }
